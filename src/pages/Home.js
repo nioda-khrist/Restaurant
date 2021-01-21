fa68370ecce8d6e6af1c096fbfe87ab53660c028
Khrist <khrist.nioda@gmail.com>
2021-01-21 14:49:30 +0800

    second commit

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,6 +2,20 @@ import React, { useEffect } from 'react';
 import axios from 'axios';
 
 const Home = () => {
+  axios({
+    method: 'GET',
+    url: 'https://developers.zomato.com/api/v2.1/restaurant?res_id=18929884',
+    headers: {
+      'user-key': process.env.REACT_APP_API_KEY,
+      'content-type': 'application/json',
+    },
+  })
+    .then((response) => {
+      console.log(response.data);
+    })
+    .catch((error) => {
+      console.log(error);
+    });
   return <div>Home</div>;
 };
 
